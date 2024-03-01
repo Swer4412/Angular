fac1714371546fc8c63b37be98afdad4a44d5e61
swer4412 <swer4412@gmail.com>
2024-03-01 23:48:37 +0100

    1.18.16

--- AngularTutorial/src/app/app.component.ts
+++ AngularTutorial/src/app/app.component.ts
@@ -2,20 +2,50 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { WishItem } from '../../share/models/wishItem';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+const filters = [
+  (item : WishItem) => item,
+  (item : WishItem) => !item.isComplete,
+  (item : WishItem) => item.isComplete
+]
 
 @Component({
   selector: 'app-root', //Questo determina come il componente puo essere richiamato es. <app-root></app-root>
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
   items : WishItem[] = [
     new WishItem("Imparare angular"),
-    new WishItem("Imparare react"),
+    new WishItem("Imparare react", true),
     new WishItem("Imparare linux")
   ]
+  
+  newWishText = "";
+
+  addNewWish() {
+    this.items.push(new WishItem(this.newWishText))
+    this.newWishText = ""
+  }
+
+  toggleItem(item: WishItem) { 
+    //Questo viene fatto perchè l'utilizzo di {{}} è un one-way binding, di conseguenza il valore registrato in html è sempre lo stesso
+    item.isComplete = !item.isComplete 
+  }
+  
+  listFilter : number = 0
+
+  //Può sembrare una funzione ma è un getter, serve per ritornare una proprietà di un oggetto (istanza di classe)
+  //Ogni volta che visibleItems viene utilizzata, viene eseguito il codice dentro le parentesi graffe
+  //Comportamento simile a python con la definizione di @getter in modo di poter fare:
+  //obj.visibleItems (proprietà) e non come in java obj.getVisibleItems() (funzione)
+  get visibleItems() : WishItem[] {
+    return this.items.filter(filters[this.listFilter]) //Al bro piace complicarsi la vita
+  }
 
 
 }
