977618936e333257ea1d801f68c2728422052a56
swer4412 <swer4412@gmail.com>
2024-03-26 22:57:24 +0100

    3.01.20

--- AngularTutorial/src/app/wish/wish-list/wish-list.component.ts
+++ AngularTutorial/src/app/wish/wish-list/wish-list.component.ts
@@ -1,19 +1,17 @@
-import { Component, Input} from '@angular/core';
-import { WishItem } from '../../../share/models/wishItem';
+import { Component, Input } from '@angular/core';
+import { WishItem } from '../../../../share/models/wishItem';
 import { CommonModule } from '@angular/common';
 import { WishListItemComponent } from '../wish-list-item/wish-list-item.component';
-
-
 
 @Component({
   selector: 'wish-list',
   standalone: true,
   imports: [CommonModule, WishListItemComponent],
   templateUrl: './wish-list.component.html',
-  styleUrl: './wish-list.component.css'
+  styleUrl: './wish-list.component.css',
 })
 export class WishListComponent {
   //Diciamo ad angular che wishes è una proprietà il cui input viene da un altro componente,
   //tipo props in react (const componente ({wishes}) => {...}), solo che in angular è bidirezionale
-  @Input() wishes : WishItem[] = []
+  @Input() wishes: WishItem[] = [];
 }
